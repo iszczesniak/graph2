169eabef31ee5e70014242528eec810aafd8ec33
Irek Szcześniak <irek.szczesniak@gmail.com>
2022-11-12 23:10:37 +0100

    test

--- test/graph.cc
+++ test/graph.cc
@@ -27,17 +27,28 @@ main()
   // Test the index traits.
   Index<vertex<edge<int, SU>>> i0 = get_index(v0);
 
-  auto &e0 = add_edge(v0, v1, 2, {CU(1, 5)});
-  auto &e1 = add_edge(v0, v2, 2, {CU(0, 3)});
-  auto &e2 = add_edge(v1, v2, 2, {CU(1, 5)});
-  auto &e3 = add_edge(v2, v3, 2, {CU(1, 5)});
-
-  cout << "&v0 = " << &v0 << endl;
-  cout << "&get_source(e0) = " << &get_source(e0) << endl;
+  add_edge(v0, v1, 1, {CU(1, 5)}); // e0
+  add_edge(v0, v2, 2, {CU(0, 3)}); // e1
+  add_edge(v1, v2, 3, {CU(1, 5)}); // e2
+  add_edge(v2, v3, 4, {CU(1, 5)}); // e3
 
   // Make sure we are using the same vertex objects.
+  auto v0i = get_edges(v0).begin();
+
+  // Edge e0.
+  auto &e0 = *v0i;
   assert(get_source(e0) == v0);
-  assert(get_target(e1) == v1);
+  assert(get_target(e0) == v1);
+  assert(get_weight(e0) == 1);
+  assert(get_resources(e0) == SU{CU(1, 5)});
+
+  // Edge e1.
+  ++v0i;
+  auto &e1 = *v0i;
+  assert(get_source(e1) == v0);
+  assert(get_target(e1) == v2);
+  assert(get_weight(e1) == 2);
+  assert(get_resources(e1) == SU{CU(0, 3)});
   
   for(const auto &v: get_vertexes(g))
     {
