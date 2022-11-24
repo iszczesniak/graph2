d93b6b30b67dbc6e19e3b2243be0705de9b0202c
Irek Szcześniak <irek.szczesniak@gmail.com>
2022-11-24 12:52:06 +0100

    props

--- props.hpp
+++ props.hpp
@@ -81,4 +81,43 @@ operator << (std::ostream &os, const resources<Resources> &w)
   return os;
 }
 
+// -------------------------------------------------------------------
+// index
+// -------------------------------------------------------------------
+
+template <typename Index>
+struct index
+{
+  Index m_index;
+
+  index(const Index &index): m_index(index)
+  {
+  }
+};
+
+// The non-const getter.
+template <typename Index>
+auto &
+get_index(index<Index> &a)
+{
+  return a.m_index;
+}
+
+// The const getter.
+template <typename Index>
+const auto &
+get_index(const index<Index> &a)
+{
+  return a.m_index;
+}
+
+template <typename Index>
+std::ostream &
+operator << (std::ostream &os, const index<Index> &w)
+{
+  os << "index = " << w.m_index;
+
+  return os;
+}
+
 #endif // PROPS_HPP
